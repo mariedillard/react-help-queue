a79794d94f7f327bc5832a92fcf5271df7d1d5c6
Guest User <Guest@epicodus-79.local>
2019-05-06 18:14:45 -0700

    finishes to lesson best practices for structuring part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,67 @@
 import React from 'react';
-import TicketList from './TicketList';
 import Header from './Header';
-import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+import Moment from 'moment';
+import Admin from './Admin';
 
-function App(){
-    return (
-        <div>
-            <Header/>
-            <Switch>
-                <Route exact path='/' component={TicketList} />
-                <Route path='/newticket' component={NewTicketForm} />
-                <Route component={Error404} />
-            </Switch>
-        </div>
-    );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            masterTicketList: [],
+            selectedTicket: null
+        };
+        this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+        this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    }
+
+    handleChangingSelectedTicket(ticket){
+        this.setState({selectedTicket: ticket});
+    }
+
+    componentDidMount() {
+        this.waitTimeUpdateTimer = setInterval(() =>
+            this.updateTicketElapsedWaitTime(),
+            5000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.waitTimeUpdateTimer);
+    }
+
+    updateTicketElapsedWaitTime() {
+        let newMasterTicketList = this.state.masterTicketList.slice();
+        newMasterTicketList.forEach((ticket) =>
+            ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+        );
+        this.setState({masterTicketList: newMasterTicketList})
+    }
+
+    handleAddingNewTicketToList(newTicket){
+        var newMasterTicketList = this.state.masterTicketList.slice();
+        newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true)
+        newMasterTicketList.push(newTicket);
+        this.setState({masterTicketList: newMasterTicketList});
+    }
+
+    render(){
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+                    <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+                    <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname} onTicketSelection={this.handleChangingSelectedTicket} selectedTicket={this.state.selectedTicket} />} />
+                    <Route component={Error404} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
